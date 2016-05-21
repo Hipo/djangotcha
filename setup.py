ae218476f09ed451f790c16390e38fc9ae2a7de3
Ümit Dincel <dincelumit@gmail.com>
2016-05-22 00:21:17 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='djangotcha',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/Hipo/djangotcha',
+    license='MIT',
+    author='umitdincel',
+    author_email='dincelumit@gmail.com',
+    description='API Benchmark Tool for Django',
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
